ee67e9f2cb8ad16523719ae4ea147c79a2dde76a
Joaquin Gomez <joaquin.gomez.arenas@gmail.com>
2017-04-26 00:31:26 +0200

    Medida venatanas

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-document.getElementById('message').innerHTML = "ChromeDev";
+function mostrarDimensiones() {
+  let anchoNavegador = document.body.clientWidth;
+  let altoNavegador = document.body.clientHeight;
+  document.getElementById('anchoNavegador').innerHTML = anchoNavegador;
+  document.getElementById('altoNavegador').innerHTML = altoNavegador;
+}
+
+
+mostrarDimensiones();
