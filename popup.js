bfcee744e91b0c21cf71ce6903c8661a1db01a59
Joaquin Gomez <joaquin.gomez.arenas@gmail.com>
2017-09-27 18:04:36 +0200

    Generador basico

--- popup.js
+++ popup.js
@@ -6,7 +6,30 @@ function mostrarDimensiones() {
   chrome.tabs.getSelected(null, function(tab){
     document.getElementById('anchoNavegador').innerHTML = tab.width;
     document.getElementById('altoNavegador').innerHTML = tab.height;
+
+
+    document.getElementById("botonGenerar").addEventListener("click", funcionGenerarPass);
+
+    function funcionGenerarPass() {
+
+      var cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+      var longitudCadena = cadena.length;
+      var pass = "";
+      var longitudPass=10;
+      var pos;
+      for(var i=1 ; i<=longitudPass ; i++){
+          pos = Math.floor((Math.random() * longitudCadena));
+          pass += cadena.substring(pos,pos+1);
+      }
+
+      document.getElementById("inputPass").value = pass;
+
+    }
+
   });
+
+
+
 }
 
 
