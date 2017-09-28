1491361b802f5e8b6595285eb26545f8abd850da
Joaquin Gomez <joaquin.gomez.arenas@gmail.com>
2017-09-28 12:40:13 +0200

    Anadido boton copiar, longitud y combiacion de caracteres (sin funcionalidad)

--- popup.js
+++ popup.js
@@ -8,23 +8,34 @@ function mostrarDimensiones() {
     document.getElementById('altoNavegador').innerHTML = tab.height;
 
 
-    document.getElementById("botonGenerar").addEventListener("click", funcionGenerarPass);
+    document.getElementById("botonGenerarPass").addEventListener("click", funcionGenerarPass);
+    document.getElementById("botonCopiarPass").addEventListener("click", funcionCopiarPass);
+
+
+
+
 
     function funcionGenerarPass() {
-
       var cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
       var longitudCadena = cadena.length;
       var pass = "";
-      var longitudPass=10;
+      var longitudPass= document.getElementById("selectLongitusPass").value;
       var pos;
       for(var i=1 ; i<=longitudPass ; i++){
           pos = Math.floor((Math.random() * longitudCadena));
           pass += cadena.substring(pos,pos+1);
       }
-
       document.getElementById("inputPass").value = pass;
-
     }
+
+    function funcionCopiarPass() {
+      console.log("COPIAR");
+    }
+
+
+
+
+
 
   });
 
