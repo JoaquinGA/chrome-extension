160de5a43bb70f9c9a58efe829c039427ba08a3a
Joaquin Gomez <joaquin.gomez.arenas@gmail.com>
2017-10-02 09:05:56 +0200

    Copiar pass

--- popup.js
+++ popup.js
@@ -10,16 +10,13 @@ function mostrarDimensiones() {
 
     document.getElementById("botonGenerarPass").addEventListener("click", funcionGenerarPass);
     document.getElementById("botonCopiarPass").addEventListener("click", funcionCopiarPass);
-
-
-
-
+    document.getElementById("botonCopiarLorem").addEventListener("click", funcionCopiarLorem);
 
     function funcionGenerarPass() {
       var cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
       var longitudCadena = cadena.length;
       var pass = "";
-      var longitudPass= document.getElementById("selectLongitusPass").value;
+      var longitudPass= document.getElementById("selectLongitudPass").value;
       var pos;
       for(var i=1 ; i<=longitudPass ; i++){
           pos = Math.floor((Math.random() * longitudCadena));
@@ -29,7 +26,17 @@ function mostrarDimensiones() {
     }
 
     function funcionCopiarPass() {
-      console.log("COPIAR");
+      var selectPass = document.getElementById("inputPass");
+      selectPass.select();
+      document.execCommand("copy");
+      console.log('pass copiado!');
+    }
+
+    function funcionCopiarLorem() {
+      var selectLorem = document.getElementById("inputLorem");
+      selectLorem.select();
+      document.execCommand("copy");
+      console.log('lorem copiado!');
     }
 
 
